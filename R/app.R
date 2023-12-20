19e5c028ab8a30a94438ecb530669ae30136380e
myrobot2020 <nikhil.exec@gmail.com>
2023-12-21 00:04:46 +0530

    c2

--- R/app.R
+++ R/app.R
@@ -1,4 +1,16 @@
 library(shiny)
+
+theUI <- function(id) {
+  tagList(
+    selectInput(NS(id, "a"), "a", choices = unique(iris$Species)),
+    selectInput(NS(id, "b"), "b", unique(names(iris)[-5])),
+    actionButton(NS(id,"c"),"c"),
+    plotOutput(NS(id,"d")),
+    plotOutput(NS(id,"e"))
+  )
+}
+
+
 irisApp <- function() {
   ui <- theUI("mod2")
   server <- function(input, output, session) {
